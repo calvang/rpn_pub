46ae39452c79f2c870e51b0016212b6776271f18
calvang <calvang@umich.edu>
2019-11-02 11:00:38 -0400

    Added power operator

--- rpn.py
+++ rpn.py
@@ -8,15 +8,20 @@ operators = {
     '-': operator.sub,
     '*': operator.mul,
     '/': operator.truediv,
+    '^': operator.pow,  
 }
 
 def calculate(myarg):
     stack = list()
+    # TODO more intelligent split
+    arg_list = split_arges(myarg)
+    # TODO convert matrix string into np.array
     for token in myarg.split():
         try:
             token = int(token)
             stack.append(token)
         except ValueError:
+            # TODO handle stack of brackets 
             function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
